917fac0dfa88280b22078dc82fe4654053f9ca1d
Kumi <64310263+KumiGhardt@users.noreply.github.com>
2021-01-10 00:08:24 +0100

    Update scripts.js
    if confirmed this commit will
    1. create a for loop to extract the names and heights of the Pokemon
    2. Use if/else statements to describe the particularity of unique pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,25 @@
-
-let pokemonList = [
-    {name: 'Pikachu', height: 0.4, types: ['electric']},
-
-    {name: 'Wigglytuff', height: 1, types: ['fairy', 'normal']},
-
-    {name: 'Gloom', height: 0.8, types: ['grass', 'poison']},
+let pokemonList = [{
+        name: "Pikachu",
+        height: 0.4,
+        types: ["electric"]
+    },
+    {
+        name: "Wigglytuff",
+        height: 1,
+        types: ["fairy", "normal"]
+    },
+    {
+        name: "Gloom",
+        height: 0.8,
+        types: ["grass", "poison"]
+    }
 ];
+
+for (let i = 0; i < pokemonList.length; i++) {
+    //target the height in the object to compose the ef-else conditions
+    if (pokemonList[i].height >= 1) {
+        document.write(`${pokemonList[i].name} (Height ${pokemonList[i].height}) - Wow, that’s big `) //used template literal form to concatenate the name and height from the array
+    } else {
+        document.write(`${pokemonList[i].name} (Height ${pokemonList[i].height}) `)
+    }
+};
