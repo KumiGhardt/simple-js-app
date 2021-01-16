802044ae2d7a65fc951478508b4138e001fa0e92
Kumi <64310263+KumiGhardt@users.noreply.github.com>
2021-01-16 20:35:21 +0100

    Update scripts.js
    If accepted, this commit uses the IIFE on the pokemonList and ands the forEach() loop to refer to the new variable that hols the IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,35 @@
-let pokemonList = [{
-        name: "Pikachu",
-        height: 0.4,
-        types: ["electric"]
-    },
-    {
-        name: "Wigglytuff",
-        height: 1,
-        types: ["fairy", "normal"]
-    },
-    {
-        name: "Gloom",
-        height: 0.8,
-        types: ["grass", "poison"]
-    },
-    {
-        name: "Blastoise",
-        height: 1.6,
-        types: ["water"]
-    }
-];
+let pokemonRepository = (function () {
+    let pokemonList = [{
+            name: "Pikachu",
+            height: 0.4,
+            types: ["electric"]
+        },
+        {
+            name: "Wigglytuff",
+            height: 1,
+            types: ["fairy", "normal"]
+        },
+        {
+            name: "Gloom",
+            height: 0.8,
+            types: ["grass", "poison"]
+        }
+    ];
 
-pokemonList.forEach(function (pokemon) {
+    function add(pokemon) {
+        pokemonList.push(pokemon);
+    }
+
+    function getAll() {
+        return pokemonList;
+    }
+    return {
+        add: add,
+        getAll: getAll
+    };
+})();
+
+pokemonRepository.getAll().forEach(function (pokemon) {
     if (pokemon.height >= 1) {
         document.write(`<div>${pokemon.name} (Height ${pokemon.height}) - Wow, that’s big</div> `) //used template literal form to concatenate the name and height from the array
     } else {
