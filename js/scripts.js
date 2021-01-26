5723da9e9b9416e2838fab8d2fe9f0e76e7a9f6a
Kumi <64310263+KumiGhardt@users.noreply.github.com>
2021-01-26 19:46:53 +0100

    adding Pokemon modals
    If agreed, this commit will:
    - add styles to the modal that brings up pokemon details
    - edit the showDetails() function to bring up the modal with pokemon details: name, height and picture

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,4 @@
 let pokemonRepository = (function () {
-
     let pokemonList = [];
     let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
@@ -13,7 +12,7 @@ let pokemonRepository = (function () {
             console.log('pokemon is not correct');
         }
     }
-
+    ///POKEMOOON LIST///////////
     function addListItem(pokemon) {
         let list = document.querySelector('.pokemon-list'); // create a variable and  assign it the ul
         let listItem = document.createElement('li'); //create li for the ul
@@ -29,20 +28,23 @@ let pokemonRepository = (function () {
     //function dedicated to adding an event listener to the newly created button 
     function buttonListener(button, pokemon) {
         button.addEventListener('click', function () {
-            showDetails(pokemon)
+            showDetails(pokemon, )
         });
     }
 
-    function showDetails(pokemon) {
-        loadDetails(pokemon).then(function () {
-            console.log(pokemon);
+    function showDetails(pokemon, details) {
+        loadDetails(pokemon).then(function (pokemonDetails) {
+            //get pokemon details and add to modal
+            showModal(pokemonDetails);
         });
+
     }
 
     function getAll() {
         return pokemonList;
     }
-    //loadlist (promise  fetch) function
+
+    //The LoadList() method will fetch data from the API, then add each Pokémon in the fetched data to pokemonList with the add function you implemented earlier. 
     function loadList() {
         return fetch(apiUrl).then(function (response) {
             return response.json();
@@ -58,7 +60,7 @@ let pokemonRepository = (function () {
             console.error(e);
         })
     }
-
+    ///You can then use the detailsUrl property to load the detailed data for a given Pokémon. For this, you add a loadDetails() function, which takes a Pokémon item as an argument:
     function loadDetails(item) {
         let url = item.detailsUrl;
         return fetch(url).then(function (response) {
@@ -68,10 +70,69 @@ let pokemonRepository = (function () {
             item.imageUrl = details.sprites.front_default;
             item.height = details.height;
             item.types = details.types;
+
+            return item;
         }).catch(function (e) {
             console.error(e);
         });
     }
+
+    //modal code
+    let modalContainer = document.querySelector('#modal-container');
+
+    function showModal(pokemonDetails) {
+        console.log(pokemonDetails);
+        modalContainer.innerHTML = '';
+        let modal = document.createElement('div');
+        modal.classList.add('modal');
+
+        let closeButtonElement = document.createElement('button');
+        closeButtonElement.classList.add('modal-close');
+        closeButtonElement.innerText = 'Close';
+        closeButtonElement.addEventListener('click', hideModal);
+
+        //add pokemon name
+        let titleElement = document.createElement('h1');
+        titleElement.innerText = pokemonDetails.name;
+
+        //add height to pokemon details
+        let contentElement = document.createElement('p');
+        contentElement.innerText = `Height:  ${pokemonDetails.height}`;
+
+        //add image to pokemon card
+        let imgElement = document.createElement('img');
+        imgElement.src = pokemonDetails.imageUrl;
+
+        modal.appendChild(closeButtonElement);
+        modal.appendChild(titleElement);
+        modal.appendChild(contentElement);
+        modal.appendChild(imgElement);
+        modalContainer.appendChild(modal);
+
+        modalContainer.classList.add('is-visible');
+
+    };
+
+    function hideModal() {
+        modalContainer.classList.remove('is-visible');
+    }
+
+
+    // modal escape key to hide the modal
+    window.addEventListener('keydown', (e) => {
+        let modalContainer = document.querySelector('#modal-container');
+        if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+            hideModal();
+        }
+    });
+
+    //event listener for clicking outside of the modal
+    modalContainer.addEventListener('click', (e) => {
+        let target = e.target;
+        if (target === modalContainer) {
+            hideModal();
+        }
+    });
 
     return {
         add: add,
@@ -81,12 +142,6 @@ let pokemonRepository = (function () {
         loadDetails: loadDetails
     };
 })();
-
-pokemonRepository.add({
-    name: "Nidoking",
-    height: 1.4,
-    types: ["ground", "poison"]
-});
 
 
 pokemonRepository.loadList().then(function () {
